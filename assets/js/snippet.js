46fc413aa6d5760effff36c31ec50fb8044cb847
prio101 <miprio101@gmail.com>
2015-02-08 23:36:50 +0600

    Progress bar in space

--- assets/js/snippet.js
+++ assets/js/snippet.js
@@ -1,4 +1,59 @@
+
 /*Js of the pie chart*/
+//
+//$(function(){
+//    $('.chart1').easyPieChart({
+//        barColor:'#000',
+//        scaleColor:false,
+//        lineWidth:5,
+//        lineCap:'circle',
+//        rotate:180
+//
+//    });
+//    $('.chart2').easyPieChart({
+//        barColor:'#000',
+//        scaleColor:false,
+//        lineWidth:5,
+//        lineCap:'circle',
+//        rotate:180
+//
+//    });
+//    $('.chart3').easyPieChart({
+//        barColor:'#000',
+//        scaleColor:false,
+//        lineWidth:5,
+//        lineCap:'circle',
+//        rotate:180
+//
+//    });
+//
+//    $('.chart4').easyPieChart({
+//        barColor:'#000',
+//        scaleColor:false,
+//        lineWidth:5,
+//        lineCap:'circle',
+//        rotate:180
+//
+//    });
+//
+//    $('.chart5').easyPieChart({
+//        barColor:'#000',
+//        scaleColor:false,
+//        lineWidth:5,
+//        lineCap:'circle',
+//        rotate:180
+//
+//    });
+//
+//
+//
+//}).onStep('#home','#about');
+//
+//
+//
+//
+
+
 
 $(function(){
     $('.chart1').easyPieChart({
@@ -44,6 +99,20 @@ $(function(){
 
     });
 
+    $('.chart6').easyPieChart({
+        barColor:'#000',
+        scaleColor:false,
+        lineWidth:5,
+        lineCap:'circle',
+        rotate:180
+
+    });
+
 
 
 });
+
+
+
+
+
