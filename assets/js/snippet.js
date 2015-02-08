63a7b0e091a697cf175858efd2e707fccc54b149
prio101 <miprio101@gmail.com>
2015-02-08 15:54:03 +0600

    Little of ability meter

---
+++ assets/js/snippet.js
@@ -0,0 +1,49 @@
+/*Js of the pie chart*/
+
+$(function(){
+    $('.chart1').easyPieChart({
+        barColor:'#000',
+        scaleColor:false,
+        lineWidth:5,
+        lineCap:'circle',
+        rotate:180
+
+    });
+    $('.chart2').easyPieChart({
+        barColor:'#000',
+        scaleColor:false,
+        lineWidth:5,
+        lineCap:'circle',
+        rotate:180
+
+    });
+    $('.chart3').easyPieChart({
+        barColor:'#000',
+        scaleColor:false,
+        lineWidth:5,
+        lineCap:'circle',
+        rotate:180
+
+    });
+
+    $('.chart4').easyPieChart({
+        barColor:'#000',
+        scaleColor:false,
+        lineWidth:5,
+        lineCap:'circle',
+        rotate:180
+
+    });
+
+    $('.chart5').easyPieChart({
+        barColor:'#000',
+        scaleColor:false,
+        lineWidth:5,
+        lineCap:'circle',
+        rotate:180
+
+    });
+
+
+
+});
